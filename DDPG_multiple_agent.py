b4eaeca9ba8fe0f551a67b548e6edb76fe09f5e5
Jax Tang <p2010497@mpu.edu.mo>
2024-01-06 12:39:41 +0800

    modify on multiple agent

--- DDPG_multiple_agent.py
+++ DDPG_multiple_agent.py
@@ -25,10 +25,8 @@ test_env = env.environment_base(numAPuser,numRU,linkmode,ru_mode)
 
 reward_history = []
 system_bitrate_history = []
-system_interfecence_history = []
 reward_ave_history = []
 system_ave_bitrate_history = []
-system_ave_interfecence_history = []
 
 agent_array = []
 #create four agents for four APs
@@ -64,14 +62,7 @@ for i_episode in range(episode):
 
         for i_agent in range(4):
             DDPG_agent = agent_array[i_agent]
-            # observation_tem = observation_array[i_agent]
             observation = observation_array[i_agent]
-
-            # observation = np.zeros((numSenario,numAPuser,numRU))
-            # observation[:]=observation_tem
-
-
-            #observation_array.append(observation)
             done = False
             action_pre = DDPG_agent.choose_action(observation,train=True)
             user_list = [1,1,1,2,2,2,3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8,9,9,9,0,0,0]
@@ -102,10 +93,8 @@ for i_episode in range(episode):
         #only work for 4-agent
         RU_mapper = np.vstack((action_array[0].reshape(1,numAPuser,numRU), action_array[1].reshape(1,numAPuser,numRU), action_array[2].reshape(1,numAPuser,numRU), action_array[3].reshape(1,numAPuser,numRU)))
         system_bitrate = test_env.calculate_4_cells(RU_mapper)
-        key_value = system_bitrate/(1e+6)-180 # 180 for (5.8)
+        key_value = system_bitrate/(1e+4)
         reward = key_value
-        # to 187000000
-        # key_value = system_bitrate/(1e+6)-180
         x_, y_ = test_env.senario_user_local_move(x,y)
         userinfo_ = test_env.senario_user_info(x_,y_)
         channel_gain_obs_ = test_env.channel_gain_calculate()
@@ -137,5 +126,5 @@ for i_episode in range(episode):
             system_ave_bitrate_history.append(system_bitrate_ave)
             print('i_episode =',i_episode, 'reward =',reward_ave, 'system_bitrate =',system_bitrate_ave)
 
-# dataframe=pd.DataFrame({'bitrate':system_ave_bitrate_history})
-# dataframe.to_csv("./result/bitrate_"+str(numAPuser)+".csv", index=False,sep=',')
+dataframe=pd.DataFrame({'bitrate':system_ave_bitrate_history})
+dataframe.to_csv("./result/bitrate_multiple_waterFilling.csv", index=False,sep=',')
