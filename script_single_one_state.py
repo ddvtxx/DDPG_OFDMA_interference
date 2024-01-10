613d3c29fdd819a7ac2f371ec8a16acdbb68a484
Jax Tang <p2010497@mpu.edu.mo>
2024-01-10 16:19:58 +0800

    change the gamma to 0 in script

--- script_single_one_state.py
+++ script_single_one_state.py
@@ -27,7 +27,7 @@ DDPG_agent = DDPG(alpha=1e-4, beta=2e-4,numSenario=numSenario,numAPuser=numAPuse
       critic_fc1_dim=2**6,critic_fc2_dim=2**7,critic_fc3_dim=2**7,
       critic_fc4_dim=2**6,
       ckpt_dir='./DDPG/',
-      gamma=0.99,tau=0.001,action_noise=1e-5,max_size=100000,batch_size=128)
+      gamma=0,tau=0.001,action_noise=1e-5,max_size=100000,batch_size=128)
 create_directory('./DDPG/',sub_paths=['Actor', 'Target_actor', 'Critic', 'Target_critic'])
 #test_name = 'tanlan_junfen'
 reward_history = []
