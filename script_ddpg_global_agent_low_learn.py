316def3454d88e2c0f49e2e0c683687017ff9b85
Jax Tang <p2010497@mpu.edu.mo>
2024-01-29 21:12:31 +0800

    change the storage name

--- script_ddpg_global_agent_low_learn.py
+++ script_ddpg_global_agent_low_learn.py
@@ -97,9 +97,9 @@ for i_loop in range(6):
         
         if i_episode % 50 == 0 and i_loop%2 == 0:
             dataframe=pd.DataFrame({'bitrate':actor_loss_history})
-            dataframe.to_csv("./result/actor_loss_sinr_single_global_2k_huge_move_loop"+str(i_loop)+"_epis"+str(i_episode)+".csv", index=False,sep=',')
+            dataframe.to_csv("./result/actor_loss_sinr_single_global_2k_low_rate_loop"+str(i_loop)+"_epis"+str(i_episode)+".csv", index=False,sep=',')
             dataframe=pd.DataFrame({'bitrate':critic_loss_history})
-            dataframe.to_csv("./result/critic_loss_sinr_single_global_2k_huge_move_loop"+str(i_loop)+"_epis"+str(i_episode)+".csv", index=False,sep=',')
+            dataframe.to_csv("./result/critic_loss_sinr_single_global_2k_low_rate_loop"+str(i_loop)+"_epis"+str(i_episode)+".csv", index=False,sep=',')
 
     dataframe=pd.DataFrame({'bitrate':system_ave_bitrate_history})
-    dataframe.to_csv("./result/bitrate_sinr_single_global_agent_2k_huge_move_"+str(i_loop)+".csv", index=False,sep=',')
+    dataframe.to_csv("./result/bitrate_sinr_single_global_agent_2k_low_rate_"+str(i_loop)+".csv", index=False,sep=',')
