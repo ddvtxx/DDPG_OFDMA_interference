2a91ab96061d3c0015a0755563b3ee296055bba2
Jax Tang <p2010497@mpu.edu.mo>
2024-02-01 01:06:06 +0800

    change the parameter

--- script_ddpg_global_agent_tau_change.py
+++ script_ddpg_global_agent_tau_change.py
@@ -96,14 +96,14 @@ for i_loop in range(6):
         
         if i_episode % 50 == 0 and i_loop%2 == 0:
             dataframe=pd.DataFrame({'bitrate':actor_loss_history})
-            dataframe.to_csv("./result/actor_loss_sinr_single_global_2k_tau_change_300_01_06_loop"+str(i_loop)+"_epis"+str(i_episode)+".csv", index=False,sep=',')
+            dataframe.to_csv("./result/actor_loss_sinr_single_global_2k_tau_change_400_01_07_loop"+str(i_loop)+"_epis"+str(i_episode)+".csv", index=False,sep=',')
             dataframe=pd.DataFrame({'bitrate':critic_loss_history})
-            dataframe.to_csv("./result/critic_loss_sinr_single_global_2k_tau_change_300_01_06_loop"+str(i_loop)+"_epis"+str(i_episode)+".csv", index=False,sep=',')
+            dataframe.to_csv("./result/critic_loss_sinr_single_global_2k_tau_change_400_01_07_loop"+str(i_loop)+"_epis"+str(i_episode)+".csv", index=False,sep=',')
 
-        if i_episode%300==0 and act_tau>0.6:
+        if i_episode%400==0 and act_tau>0.7:
             act_tau = act_tau - 0.1
             DDPG_agent.change_act_tau(act_tau)
 
 
     dataframe=pd.DataFrame({'bitrate':system_ave_bitrate_history})
-    dataframe.to_csv("./result/bitrate_sinr_single_global_agent_2k_tau_change_300_01_06_"+str(i_loop)+".csv", index=False,sep=',')
+    dataframe.to_csv("./result/bitrate_sinr_single_global_agent_2k_tau_change_400_01_07_"+str(i_loop)+".csv", index=False,sep=',')
