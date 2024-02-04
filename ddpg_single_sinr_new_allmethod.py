5eb2f47f1c6b5c3530c2685d8f0a9949de7f0ebb
Jax Tang <p2010497@mpu.edu.mo>
2024-02-04 15:58:44 +0800

    change the storage name

--- ddpg_single_sinr_new_allmethod.py
+++ ddpg_single_sinr_new_allmethod.py
@@ -111,9 +111,9 @@ for i_seed in range(13,20):
 
             if i_episode % 50 == 0:
                 dataframe=pd.DataFrame({'bitrate':actor_loss_history})
-                dataframe.to_csv("./result/ddpg_actor_loss_sinr_single_sinr_no_wf_seed_"+str(i_seed)+"_loop_"+str(i_loop)+"_episode_"+str(i_episode)+".csv", index=False,sep=',')
+                dataframe.to_csv("E:/FYP/Modification Code/DDPG_OFDMA_interference/result/ddpg_actor_loss_sinr_single_sinr_no_wf_seed_"+str(i_seed)+"_loop_"+str(i_loop)+"_episode_"+str(i_episode)+".csv", index=False,sep=',')
                 dataframe=pd.DataFrame({'bitrate':critic_loss_history})
-                dataframe.to_csv("./result/ddpg_critic_loss_sinr_single_sinr_no_wf_seed_"+str(i_seed)+"_loop_"+str(i_loop)+"_episode_"+str(i_episode)+".csv", index=False,sep=',')
+                dataframe.to_csv("E:/FYP/Modification Code/DDPG_OFDMA_interference/result/ddpg_critic_loss_sinr_single_sinr_no_wf_seed_"+str(i_seed)+"_loop_"+str(i_loop)+"_episode_"+str(i_episode)+".csv", index=False,sep=',')
 
         dataframe=pd.DataFrame({'bitrate':system_ave_bitrate_history})
-        dataframe.to_csv("./result/ddpg_bitrate_single_sinr_no_wf_seed_"+str(i_seed)+"_loop_"+str(i_loop)+".csv", index=False,sep=',')
+        dataframe.to_csv("E:/FYP/Modification Code/DDPG_OFDMA_interference/result/ddpg_bitrate_single_sinr_no_wf_seed_"+str(i_seed)+"_loop_"+str(i_loop)+".csv", index=False,sep=',')
