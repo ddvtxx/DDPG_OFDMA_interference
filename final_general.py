4aa56a3de12e705d40b1110abf1ddd6152d14b4a
Jax Tang <p2010497@mpu.edu.mo>
2024-03-14 14:21:55 +0800

    modify bug

--- final_general.py
+++ final_general.py
@@ -13,17 +13,16 @@ test_env = env.environment_base(numAPuser,numRU,linkmode,ru_mode,seed=9)
 test_env.change_RU_mode(4)
 system_bitrate_history = []
 system_ave_bitrate_history = []
+x_init,y_init = test_env.senario_user_local_init()
+x,y = x_init,y_init
 for i_iteration in range(max_iteration):
-    x_init,y_init = test_env.senario_user_local_init()
-    x,y = x_init,y_init
     userinfo = test_env.senario_user_info(x,y)
     channel_gain_obs = test_env.channel_gain_calculate()
-    # RU_mapper = test_env.n_AP_RU_mapper()
     RU_mapper = np.zeros((numSenario, numAPuser, numRU))
     for i_action in range(4):
         RU_mapper[i_action,:,:]=(test_env.allocate_RUs(channel_gain_obs[i_action][i_action]))
-    print(RU_mapper)
-    system_bitrate = test_env.calculate_4_cells_without_wf(RU_mapper)
+    # print(RU_mapper)
+    system_bitrate = test_env.calculate_4_cells(RU_mapper)
     x_,y_ = test_env.senario_user_local_move(x,y)
     x, y =x_, y_
     system_bitrate_history.append(system_bitrate)
